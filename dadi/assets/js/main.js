b60e6cc9fc17c34ed3dd17e979b1985e84be2df9
Andrea Di Cataldi <andrea.dicataldi@gmail.com>
2020-11-17 15:46:24 +0100

    Final 1.0
    Removed console logs

--- dadi/assets/js/main.js
+++ dadi/assets/js/main.js
@@ -1,11 +1,8 @@
-alert("Lancia il dado e prova a battere il PC!")
+alert("Lancia il dado e prova a battere il PC!");
 
-// Genera un numero random da 1 a 6
 var firstRandomNum = Math.floor(Math.random() * 6) + 1;
-console.log(firstRandomNum);
 
 var secondRandomNum = Math.floor(Math.random() * 6) + 1;
-console.log(secondRandomNum);
 
 document.getElementById("player-dice").innerHTML = firstRandomNum;
 document.getElementById("pc-dice").innerHTML = secondRandomNum;
@@ -15,5 +12,5 @@ if (firstRandomNum > secondRandomNum) {
 } else if (firstRandomNum < secondRandomNum) {
   alert("Hai perso!");
 } else {
-  alert("E' un pareggio!")
+  alert("E' un pareggio!");
 }

7de97a88a8ac80425e40be451f4b078a5d8fa5c9
Andrea Di Cataldi <andrea.dicataldi@gmail.com>
2020-11-17 15:48:41 +0100

    Enhanced dadi

--- dadi/assets/js/main.js
+++ dadi/assets/js/main.js
@@ -4,6 +4,9 @@ var firstRandomNum = Math.floor(Math.random() * 6) + 1;
 
 var secondRandomNum = Math.floor(Math.random() * 6) + 1;
 
+alert("Il risultato del tuo lancio è: " + firstRandomNum);
+alert("Il risultato del lancio del PC è: " + secondRandomNum);
+
 document.getElementById("player-dice").innerHTML = firstRandomNum;
 document.getElementById("pc-dice").innerHTML = secondRandomNum;
 
